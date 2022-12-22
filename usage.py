36863366ba076c6180101a1fed4269fed498b8fc
r4nd0lph <r11345roma@yandex.ru>
2022-12-23 00:41:28 +0300

    updated structure & usage.py

--- usage.py
+++ usage.py
@@ -2,16 +2,18 @@
 
 
 import json
-from mospolytech_api import api, group, schedule
+from mospolytech_api.api import API
+from mospolytech_api.group import Group
+from mospolytech_api.schedule import Schedule
 
 
 if __name__ == "__main__":
-    # API() initialization
-    api = api.API()
+    # API initialization
+    api = API()
 
     # getting all available groups
-    list_groups = api.get_groups()
-    print(f"\n\nGROUPS:\n{list_groups}\n\nCOUNT:\n{len(list_groups)}")
+    groups = api.get_groups()
+    print(f"\n\nGROUPS:\n{groups}\n\nCOUNT:\n{len(groups)}")
 
     # getting a sorted list of students from parallel groups
     list_students = api.get_students([
@@ -20,22 +22,28 @@ if __name__ == "__main__":
     ])
     print(f"\n\nSTUDENTS:\n{list_students}\n\nCOUNT:\n{len(list_students)}")
 
-    # getting the raw shedule of the choosen group
-    dict_schedule = api.get_schedule("201-721")
-    print(f"\n\nSCHEDULE:\n{dict_schedule}")
+    # getting raw shedule of specified group
+    raw_schedule = api.get_schedule("201-721")
+    print(f"\n\nSCHEDULE:\n{raw_schedule}")
 
-    # saving the raw schedule to JSON file
-    with open("logs_raw_schedule.json", "w", encoding="utf-8") as f:
-        json.dump(dict_schedule, f, ensure_ascii=False, indent=4)
+    # saving raw_schedule to JSON file
+    with open("raw_schedule.json", "w", encoding="utf-8") as f:
+        json.dump(raw_schedule, f, ensure_ascii=False, indent=4)
 
-    # Group() object initialization
-    # group_obj = group.Group()
+    # Group object initialization
+    group_obj = Group()
 
     # ...
     # ...
     # ...
 
-    # Schedule() object initialization
-    schedule_obj = schedule.Schedule(dict_schedule)
+    # Schedule object initialization
+    schedule_obj = Schedule(raw_schedule)
+
+    # getting schedule on specified day
     day = schedule_obj.get_day("12.12.2022")
     print(f"\n\nGROUP {schedule_obj.group}, DAY {day['date']}:\n{day['body']}")
+
+    # saving dau to JSON file
+    with open("day.json", "w", encoding="utf-8") as f:
+        json.dump(day, f, ensure_ascii=False, indent=4)
